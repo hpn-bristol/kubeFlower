55682485c208989497af040ab61a8c0df57cfe03
Ubuntu <luisfgon@it.uc3m.es>
2023-05-11 16:16:24 +0000

    kubeFlower operator for multiple clients working

--- dockerfile
+++ dockerfile
@@ -4,4 +4,5 @@ RUN /usr/local/bin/python -m pip install --upgrade pip
 COPY ./requirements.txt .
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
 COPY ./src src
+RUN mkdir data
 CMD ["/bin/sh", "-c", "while sleep 1000; do :; done"]
--- operator/fl_operator.py
+++ operator/fl_operator.py
@@ -12,21 +12,26 @@ custom_api = client.CustomObjectsApi()
 def create_fldeployment(spec, **kwargs):
     server_spec = spec.get('server', {})
     client_spec = spec.get('client', {})
-    
     server_image = server_spec.get('image', 'juanmarcelouob/kubeflower:latest')
     server_image_pull_policy = server_spec.get('imagePullPolicy', 'IfNotPresent')
-    server_port = server_spec.get('port', 80)
+    server_port = server_spec.get('port', 8080)
     server_replicas = server_spec.get('replicas', 1)
     #########POTENTIALLY MAKE THE SERVER TO ORCHESTRATE THE PRIVACY BUDGET- FA TO KEEP TRACK OF THE BUDGET
-    
     client_image = client_spec.get('image', 'juanmarcelouob/kubeflower:latest')
-    client_image_pull_policy = client_spec.get('imagePullPolicy', 'IfNotPresent')
+    client_image_pull_policy = client_spec.get('imagePullPolicy', 'Always')
     client_port = client_spec.get('port', 30050)
     client_replicas = client_spec.get('replicas', 1)
     client_args = client_spec.get('args', [])
+    client_dataset = client_spec.get('dataset', {})
+    client_dataset_path = client_dataset.get('path', './data')
+    client_dataset_download = client_dataset.get('download', True)
     client_privacy = client_spec.get('privacy', {})
-    client_privacy_budget = client_privacy.get('budget', 1.0)
-    client_privacy_rate = client_privacy.get('rate',1.0)
+    if len(client_privacy) == 0:
+        print("You haven't add privacy values")
+    else:
+        client_privacy_budget = client_privacy.get('budget', 1.0)
+        client_privacy_rate = client_privacy.get('rate',1.0)
+    #Server deployment 
     server_deployment_body = {
         "apiVersion": "apps/v1",
         "kind": "Deployment",
@@ -87,47 +92,7 @@ def create_fldeployment(spec, **kwargs):
             ]
         }
     }
-    persisten_volume_body = {
-        "apiVersion":"v1",
-        "kind":"PersistentVolume",
-        "metadata": {
-            "name":f"my-pv",
-            "labels":{
-                "type": "local"
-            }
-        },
-        "spec":{
-            "storageClassName" : "manual",
-            "capacity":{
-                "storage":"1Gi"
-            },
-            "accessModes":[
-                "ReadWriteOnce"
-            ],
-            "hostPath": {
-                "path": "/home/ubuntu/git/kubeFlower/data"
-            }
-        }
-    }
-    persisten_volume_claim_body = {
-        "apiVersion":"v1",
-        "kind":"PersistentVolumeClaim",
-        "metadata": {
-            "name":f"my-pvc",
-            "namespace": kwargs['body']['metadata']['namespace']
-        },
-        "spec":{
-            "storageClassName" : "manual",
-            "resources":{
-                "requests": {
-                    "storage":"1Gi"
-                    }
-            },
-            "accessModes":[
-                "ReadWriteOnce"
-            ]
-        }
-    }
+
     client_deployment_body = {
         "apiVersion": "apps/v1",
         "kind": "Deployment",
@@ -155,33 +120,19 @@ def create_fldeployment(spec, **kwargs):
                             "image": client_image,
                             "imagePullPolicy": client_image_pull_policy,
                             "command": ["/bin/sh", "-c"],
-                            "args": [f"ls; python ./src/client.py --server {kwargs['body']['metadata']['name']}-service-server --port {client_port}"],
+                            "args": [f"ls; cd src; ls; cd ..; python ./src/client.py --server {kwargs['body']['metadata']['name']}-service-server --port {client_port} --download {client_dataset_download}"],
                             "ports": [
                                 {
                                     "containerPort": client_port
                                 }
                             ],
-                            "volumeMounts":[
-                                {
-                                    "name": "my-volume",
-                                    "mountPath": "/data/"
-                                }
-                            ]
                         }
                     ],
-                    "volumes": [
-                        {
-                            "name": "my-volume",
-                            "persistentVolumeClaim": {
-                                "claimName": "my-pvc"
-                            }
-                        }
-
-                    ]
                 }
             }
         }
     }
+    
     #Create Flower service server
     try:
         v1.create_namespaced_service(
@@ -202,24 +153,82 @@ def create_fldeployment(spec, **kwargs):
         logging.error(f"Error creating {kwargs['body']['metadata']['name']}-server. Reason: {e}")
         pass
 
-    #Create Persistent volume for flower client
-    try:
-        v1.create_persistent_volume(
-            body=persisten_volume_body
-        )
-    except Exception as e:
-        logging.error(f"Error creating persistent volume. Reason: {e}")
-        pass
+    #Create PV and PVCs if the user wants to load data from dataset.path. They have to be created before the deployment/pod that is going to consume them
+    if not client_dataset_download:
+        #Configuring the client descriptor to add the volume claims info. *****STILL MISSING A FOR LOOP TO ITERATE OVER PVCs
+        valuesVolumeMounts = [
+                                {
+                                    "name": "my-volume",
+                                    "mountPath": "/app/data/"
+                                }
+                            ]
+        valuesVolumes = [
+                        {
+                            "name": "my-volume",
+                            "persistentVolumeClaim": {
+                                "claimName": f"{kwargs['body']['metadata']['name']}-client-pvc"
+                            }
+                        }
+                    ]
+        client_deployment_body.get('spec').get('template').get('spec').get('containers')[0]['volumeMounts'] = valuesVolumeMounts
+        client_deployment_body.get('spec').get('template').get('spec')['volumes'] = valuesVolumes    
+        persisten_volume_body = {
+            "apiVersion":"v1",
+            "kind":"PersistentVolume",
+            "metadata": {
+                "name":f"{kwargs['body']['metadata']['name']}-client-pv"
+            },
+            "spec":{
+                "storageClassName" : "manual",
+                "capacity":{
+                    "storage":"1Gi"
+                },
+                "accessModes":[
+                    "ReadWriteOnce"
+                ],
+                "hostPath": {
+                    "path": client_dataset_path
+                }
+            }
+        }
+        persisten_volume_claim_body = {
+            "apiVersion":"v1",
+            "kind":"PersistentVolumeClaim",
+            "metadata": {
+                "name":f"{kwargs['body']['metadata']['name']}-client-pvc",
+                "namespace": kwargs['body']['metadata']['namespace']
+            },
+            "spec":{
+                "storageClassName" : "manual",
+                "resources":{
+                    "requests": {
+                        "storage":"1Gi"
+                        }
+                },
+                "accessModes":[
+                    "ReadWriteOnce"
+                ]
+            }
+        }
 
-    #Create Persistent volume claim for flower client
-    try:
-        v1.create_namespaced_persistent_volume_claim(
-            namespace=kwargs['body']['metadata']['namespace'],
-            body=persisten_volume_claim_body
-        )
-    except Exception as e:
-        logging.error(f"Error creating persistent volume claim. Reason: {e}")
-        pass
+        #Create Persistent volume for flower client
+        try:
+            v1.create_persistent_volume(
+                body=persisten_volume_body
+            )
+        except Exception as e:
+            logging.error(f"Error creating persistent volume. Reason: {e}")
+            pass
+
+        #Create Persistent volume claim for flower client
+        try:
+            v1.create_namespaced_persistent_volume_claim(
+                namespace=kwargs['body']['metadata']['namespace'],
+                body=persisten_volume_claim_body
+            )
+        except Exception as e:
+            logging.error(f"Error creating persistent volume claim. Reason: {e}")
+            pass
 
     #Create Flower client
     try:
